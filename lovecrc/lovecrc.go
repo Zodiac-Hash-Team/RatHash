b8dd32046c17f378feaa52b8ab92563f0a7329d9
Matthew R Bonnette <mattrbonnette@pm.me>
2021-07-01 21:40:11 -0400

    InDev 6.5
    Now utilizes 32-bit words and 32-bit CRC checksums.

--- lovecrc/lovecrc.go
+++ lovecrc/lovecrc.go
@@ -3,14 +3,15 @@ package lovecrc
 import (
 	"encoding/base64"
 	"fmt"
-	"hash/crc64"
+	"hash/crc32"
+	"math/big"
 	"strconv"
 )
 
-/* N.B.: This project is currently InDev. */
+// N.B.: This project is currently InDev. © 2021 Matthew R Bonnette
 /* This file is the reference Go implementation of the LoveCRC hash function as contrived
-by its original author. © 2021 Matthew R Bonnette. The developer thanks The Go Authors
-and the developers of its respective libraries, especially those utilized herein. */
+by its original author. LoveCRC's developer thanks The Go Authors and the developers of
+its respective libraries, especially those utilized herein. */
 
 func Hash(msg []byte, ln int) string {
 	/* Checks that the requested digest length meets the function's requirements */
@@ -24,7 +25,7 @@ func Hash(msg []byte, ln int) string {
 	// EXPANSION FUNCTION
 	msg = append(msg, 0x80)
 	encoded := msg
-	for len(encoded) < ln/8 || len(encoded)%2 != 0 {
+	for len(encoded) < ln/8 || len(encoded)%4 != 0 {
 		encoded = []byte(base64.StdEncoding.EncodeToString(encoded))
 	}
 	expanded := string(encoded)
@@ -33,16 +34,13 @@ func Hash(msg []byte, ln int) string {
 	// COMPRESSION FUNCTION
 	var split []string
 	for len(expanded) != 0 {
-		split = append(split, expanded[:2])
-		expanded = expanded[2:]
+		split = append(split, expanded[:4])
+		expanded = expanded[4:]
 	}
+	block := make([]uint32, len(split))
 	for dex := range split {
-		split[dex] = fmt.Sprintf("%x", split[dex])
-	}
-	block := make([]uint16, len(split))
-	for dex := range split {
-		tmp, _ := strconv.ParseUint(split[dex], 16, 16)
-		block[dex] = uint16(tmp)
+		word, _ := strconv.ParseUint(fmt.Sprintf("%x", split[dex]), 16, 32)
+		block[dex] = uint32(word)
 	}
 	/* Because the first index is 0 */
 	ultima := len(block) - 1
@@ -52,41 +50,32 @@ func Hash(msg []byte, ln int) string {
 		ultima--
 		penult--
 	}
-	block = block[:ln/16]
+	block = block[:ln/32]
 
 	// PRIMALITY-BASED PROCESSING
 	for dex := range block {
-		index := 0
-		for primes[index] > block[dex] {
-			index++
+		word := int64(block[dex])
+		for big.NewInt(word).ProbablyPrime(1) != true {
+			word--
 		}
-		block[dex] = primes[index]
-	}
-
-	// DIGEST FORMATION
-	polys := make([]uint64, ln/64)
-	for dex := range polys {
-		polys[dex] =
-			uint64(block[0+dex*4])<<48 +
-				uint64(block[1+dex*4])<<32 +
-				uint64(block[2+dex*4])<<16 +
-				uint64(block[3+dex*4])
+		block[dex] = uint32(word)
 	}
 	// Debugging
 	fmt.Printf("polys:\n")
-	for dex := range polys {
-		poly := polys[dex]
+	for dex := range block {
+		poly := block[dex]
 		fmt.Printf("%x ", poly)
 	}
 	fmt.Printf("\n")
 
-	for dex := range polys {
-		table := crc64.MakeTable(polys[dex])
-		polys[dex] = crc64.Checksum(msg, table)
+	// DIGEST FORMATION
+	for dex := range block {
+		table := crc32.MakeTable(block[dex])
+		block[dex] = crc32.Checksum(msg, table)
 	}
 	var digest string
-	for index := range polys {
-		digest += fmt.Sprintf("%x", polys[index])
+	for dex := range block {
+		digest += fmt.Sprintf("%x", block[dex])
 	}
 	return digest
 }
