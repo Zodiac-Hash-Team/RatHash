3974578763e5bf49344f0654f080aba04e536b0d
Matthew R Bonnette <mattrbonnette@pm.me>
2021-08-02 15:53:46 -0400

    v0.2.0

--- rathash/rat.go
+++ rathash/rat.go
@@ -8,7 +8,7 @@ import (
 )
 
 // N.B.: This project is currently InDev.
-// Copyright © 2021 Matthew R Bonnette. Openly-licensed under a BSD-3-Clause license.
+// Copyright © 2021 Matthew R Bonnette. Licensed under a BSD-3-Clause license.
 /* This file is the reference Go implementation of the RatHash function as contrived by its original
 author. RatHash's developer thanks The Go Authors and the developers of any third-party libraries
 utilized in this project. */
@@ -30,6 +30,7 @@ func Sum(msg []byte, ln int) []byte {
 	// EXTENSION *OR* INITIALIZATION
 	const ceiling = len(primes) - 1
 	product, prime, two := big.NewInt(0), big.NewInt(0), big.NewInt(2)
+
 	if mSize < ln>>1 {
 		product.SetBytes(msg)
 		/* Makes input zero-insensitive */
@@ -71,66 +72,79 @@ func Sum(msg []byte, ln int) []byte {
 	}
 
 	// PARALLEL COMPRESSION
-	mSize = len(msg)
-	bSize := (mSize + mSize%(ln>>6)) / (ln >> 6) /* len(msg)/(ln/64) rounded up */
-	rem := bSize & 7
+	mSize = len(msg) /* Updates mSize in the case that it changed */
+	bSize := (mSize / (ln >> 3)) << 3
+	mRem := mSize - (ln >> 6 * bSize)
+	rem := mRem & 7
+
 	for i := ln>>6 - 1; i >= 0; i-- {
 		group.Add(1)
 		go func(i int) {
-			var val, pop, sum uint64
+			var word, hi, lo uint64
+			bRem, sum := mRem/8, sums[i]
 
-			switch rem {
-			case 7:
-				val = bits.Reverse64(uint64(msg[bSize*(i+1)-8])<<56) ^ phiE19 + sums[i]
-			case 6:
-				val = bits.Reverse64(
-					uint64(msg[bSize*(i+1)-8])<<56|
-						uint64(msg[bSize*(i+1)-7])<<48) ^ phiE19 + sums[i]
-			case 5:
-				val = bits.Reverse64(
-					uint64(msg[bSize*(i+1)-8])<<56|
-						uint64(msg[bSize*(i+1)-7])<<48|
-						uint64(msg[bSize*(i+1)-6])<<40) ^ phiE19 + sums[i]
-			case 4:
-				val = bits.Reverse64(
-					uint64(msg[bSize*(i+1)-8])<<56|
-						uint64(msg[bSize*(i+1)-7])<<48|
-						uint64(msg[bSize*(i+1)-6])<<40|
-						uint64(msg[bSize*(i+1)-5])<<32) ^ phiE19 + sums[i]
-			case 3:
-				val = bits.Reverse64(
-					uint64(msg[bSize*(i+1)-8])<<56|
-						uint64(msg[bSize*(i+1)-7])<<48|
-						uint64(msg[bSize*(i+1)-6])<<40|
-						uint64(msg[bSize*(i+1)-5])<<32|
-						uint64(msg[bSize*(i+1)-4])<<24) ^ phiE19 + sums[i]
-			case 2:
-				val = bits.Reverse64(
-					uint64(msg[bSize*(i+1)-8])<<56|
-						uint64(msg[bSize*(i+1)-7])<<48|
-						uint64(msg[bSize*(i+1)-6])<<40|
-						uint64(msg[bSize*(i+1)-5])<<32|
-						uint64(msg[bSize*(i+1)-4])<<24|
-						uint64(msg[bSize*(i+1)-3])<<16) ^ phiE19 + sums[i]
-			case 1:
-				val = bits.Reverse64(
-					uint64(msg[bSize*(i+1)-8])<<56|
-						uint64(msg[bSize*(i+1)-7])<<48|
-						uint64(msg[bSize*(i+1)-6])<<40|
-						uint64(msg[bSize*(i+1)-5])<<32|
-						uint64(msg[bSize*(i+1)-4])<<24|
-						uint64(msg[bSize*(i+1)-3])<<16|
-						uint64(msg[bSize*(i+1)-2])<<8) ^ phiE19 + sums[i]
-			default:
-				val = *(*uint64)(unsafe.Pointer(&msg[bSize*(i+1)-7])) + sums[i]
+			if i == ln>>6-1 {
+				switch rem {
+				case 7:
+					word = bits.Reverse64(uint64(msg[bSize*(i+1)-7])<<56) ^ phiE19
+				case 6:
+					word = bits.Reverse64(
+						uint64(msg[bSize*(i+1)-7])<<56|
+							uint64(msg[bSize*(i+1)-6])<<48) ^ phiE19
+				case 5:
+					word = bits.Reverse64(
+						uint64(msg[bSize*(i+1)-7])<<56|
+							uint64(msg[bSize*(i+1)-6])<<48|
+							uint64(msg[bSize*(i+1)-5])<<40) ^ phiE19
+				case 4:
+					word = bits.Reverse64(
+						uint64(msg[bSize*(i+1)-7])<<56|
+							uint64(msg[bSize*(i+1)-6])<<48|
+							uint64(msg[bSize*(i+1)-5])<<40|
+							uint64(msg[bSize*(i+1)-4])<<32) ^ phiE19
+				case 3:
+					word = bits.Reverse64(
+						uint64(msg[bSize*(i+1)-7])<<56|
+							uint64(msg[bSize*(i+1)-6])<<48|
+							uint64(msg[bSize*(i+1)-5])<<40|
+							uint64(msg[bSize*(i+1)-4])<<32|
+							uint64(msg[bSize*(i+1)-3])<<24) ^ phiE19
+				case 2:
+					word = bits.Reverse64(
+						uint64(msg[bSize*(i+1)-7])<<56|
+							uint64(msg[bSize*(i+1)-6])<<48|
+							uint64(msg[bSize*(i+1)-5])<<40|
+							uint64(msg[bSize*(i+1)-4])<<32|
+							uint64(msg[bSize*(i+1)-3])<<24|
+							uint64(msg[bSize*(i+1)-2])<<16) ^ phiE19
+				case 1:
+					word = bits.Reverse64(
+						uint64(msg[bSize*(i+1)-7])<<56|
+							uint64(msg[bSize*(i+1)-6])<<48|
+							uint64(msg[bSize*(i+1)-5])<<40|
+							uint64(msg[bSize*(i+1)-4])<<32|
+							uint64(msg[bSize*(i+1)-3])<<24|
+							uint64(msg[bSize*(i+1)-2])<<16|
+							uint64(msg[bSize*(i+1)-1])<<8) ^ phiE19
+				default:
+					word = *(*uint64)(unsafe.Pointer(&msg[bSize*(i+1)-7]))
+				}
+			} else {
+				bRem = 0
 			}
-			pop = uint64(bits.OnesCount64(val))
-			sum ^= pop * pop * val
+			for i2 := bits.OnesCount64(word); i2 > 0; i2-- {
+				hi, lo = bits.Mul64(word, word)
+				word = hi<<32 | lo>>32
+			}
+			sum ^= word
 
-			for i2 := bSize>>3 - 2; i2 >= 0; i2-- {
-				val = *(*uint64)(unsafe.Pointer(&msg[i*bSize+i2<<3])) + sums[i]
-				pop = uint64(bits.OnesCount64(val))
-				sum ^= pop * pop * val
+			for i2 := bSize>>3 + bRem - 2; i2 >= 0; i2-- {
+				word = *(*uint64)(unsafe.Pointer(&msg[i*bSize+i2<<3]))
+				for i3 := bits.OnesCount64(word); i3 > 0; i3-- {
+					hi, lo = bits.Mul64(word, word)
+					word = hi<<32 | lo>>32
+				}
+				sum ^= word
 			}
 			sums[i] = sum
 			group.Done()
