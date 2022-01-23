ee3fe0c8ddfbe922261df75f8493fe628ce46bc2
Matthew R Bonnette <mattrbonnette@pm.me>
2022-01-23 09:03:44 -0500

    Remove unneeded ASM and cachefiles.

--- api/asm_arm64.go
+++
@@ -1,8 +0,0 @@
-package api
-
-// Copyright © 2022 Matthew R Bonnette. Licensed under the Apache-2.0 license.
-
-const hasPopcntASM = 1
-
-//go:noescape
-func popcnt(src *byte, len uint64) (ret uint64)
